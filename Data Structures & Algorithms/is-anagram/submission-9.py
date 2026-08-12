d3721dc7231af29b035ad40853b9c4b9890cd599
Rajeshneti <lakshmirajesh.neti@gmail.com>
2026-08-12 09:44:30 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mp = {}
+        for ch in s:
+            if ch not in mp:
+                mp[ch] = 1
+            else:
+                mp[ch] +=1
+        for ch in t:
+            if ch not in mp or mp[ch] <=0:
+                return False
+            mp[ch] -=1
+        for key, val in mp.items():
+            if val != 0:
+                return False
+        return True
+        
+        
